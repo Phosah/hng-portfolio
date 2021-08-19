9c9d9017e4530456dce959555a350378a806b06b
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-08-19 15:45:46 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#999999',
+        secondary: '#6D6D6D',
+        tertiary: '#7E7C7C'
+      },
+      fontFamily: {
+        body: ['Nunito Sans'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
